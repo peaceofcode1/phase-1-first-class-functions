a35e82795e3ce9de74221539a74d5fc8fb64e94c
jafar bino <binocoder@gmail.com>
2023-10-08 20:25:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+function receivesAFunction(callback) {
+   
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+   
+    function namedFunction() {
+     
+    }
+  
+    
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    
+    return function () {
+    
+    };
+  }
+  
